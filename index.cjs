c45d34b4007daf22cec1a20f7609bd31772f2baa
Glen Hughes <glen.866@gmail.com>
2023-11-17 14:35:25 +0000

    Pass ua to ua parser

--- index.cjs
+++ index.cjs
@@ -2,7 +2,7 @@ const prompts = require('prompts');
 const UAParser = require('ua-parser-js');
 
 (async () => {
-  await prompts({
+  const response = await prompts({
     type: 'text',
     name: 'ua',
     message: 'Enter full user agent',
@@ -11,7 +11,7 @@ const UAParser = require('ua-parser-js');
 
   // console.log(response);
   try {
-    const ua = UAParser(rawUA);
+    const ua = UAParser(response.ua);
     console.log(`Result: ${ua.os.name} ${ua.os.version}, ${ua.browser.name} ${ua.browser.version}`)
   } catch (e) {
     console.error(`Fatal error: ${e}`);
