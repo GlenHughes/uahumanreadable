55532c7f93270a589ccb3132fe4054da3073ff21
Glen Hughes <glen.866@gmail.com>
2023-11-17 19:40:47 +0000

    Add device details

--- index.cjs
+++ index.cjs
@@ -12,7 +12,14 @@ const UAParser = require('ua-parser-js');
   // console.log(response);
   try {
     const ua = UAParser(response.ua);
-    console.log(`Result: ${ua.os.name} ${ua.os.version}, ${ua.browser.name} ${ua.browser.version}`)
+    const osName = ua.os.name === undefined ? 'Unknown' : ua.os.name;
+    const osVersion = ua.os.version === undefined ? 'Unknown' : ua.os.version;
+    const device = ua.device.type === undefined ? 'Desktop' : ua.device.type;
+    const model = ua.device.model === undefined ? '' : ua.device.model;
+    const vendor = ua.device.vendor === undefined ? '' : ua.device.vendor;
+    const humanReadable = `Result: ${osName} ${osVersion}, ${ua.browser.name} ${ua.browser.version}, ${device} ${model} ${vendor}`
+    // Log to terminal
+    console.log(humanReadable)
   } catch (e) {
     console.error(`Fatal error: ${e}`);
   }
